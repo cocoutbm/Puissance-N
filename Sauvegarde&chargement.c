a54ddfa0279e4eabf0e488ebdd3989b60ea7a2c6
Robin-22 <50079952+Robin-22@users.noreply.github.com>
2021-06-03 16:19:26 +0200

    Update Sauvegarde&chargement.c

--- Sauvegarde&chargement.c
+++ Sauvegarde&chargement.c
@@ -8,10 +8,7 @@
 #include <stdlib.h>
 
 void charger_partie(FILE *fichier, Grid grille) {
-    char ligne[100];
-    int l, h, taille;
-    int size = grille.hauteur * grille.largeur;
-
+    int l, h;
 
     for (l = 0; l < grille.largeur; l++) {
         for (h = 0; h < grille.hauteur; h++) {
@@ -20,28 +17,6 @@ void charger_partie(FILE *fichier, Grid grille) {
     }
 }
 
-void ouverture_fichier(Grid grille, char prenom1[], char prenom2[], char ligne[], int player, int retrait, int nb_jetons, int size){
-    FILE* fichier = fopen("fichier.txt", "r");
-    if(fichier != NULL) {
-        fgets(prenom1, 100, fichier);
-        fgets(prenom2, 100, fichier);
-        fgets(ligne, 100, fichier);
-        player =atoi(ligne);
-        fgets(ligne, 100, fichier);
-        retrait =atoi(ligne);
-        fgets(ligne, 100, fichier);
-        nb_jetons =atoi(ligne);
-        fgets(ligne, 100, fichier);
-        size = atoi(ligne);
-
-        init_grid(&grille, size);
-
-        init_grid(&grille, size);
-
-        charger_partie(fichier,grille);
-    }
-    fclose(fichier);
-}
 
 void save (Grid grille, int nb_jetons, char prenom1[], char prenom2[],int player, int retrait){
     int l = 0 , h = 0;
