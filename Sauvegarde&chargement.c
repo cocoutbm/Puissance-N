5e646cd4658e8068c627a71d0ecd966efa0ecd95
Robin-22 <50079952+Robin-22@users.noreply.github.com>
2021-06-10 14:46:52 +0200

    Update Sauvegarde&chargement.c

--- Sauvegarde&chargement.c
+++ Sauvegarde&chargement.c
@@ -3,20 +3,36 @@
 //
 
 #include "Sauvegarde&chargement.h"
+#include "Jouer.h"
+#include "Grille.h"
 #include <stdio.h>
-
-void charger_partie(FILE *fichier, Grid grille) {
-    char ligne[100];
-    int l, h, taille;
-    int size = grille.hauteur * grille.largeur;
+#include <stdlib.h>
 
 
-    for (l = 0; l < grille.largeur; l++) {
-        for (h = 0; h < grille.hauteur; h++) {
-            fgets(&grille.tableau[l][h], 10, fichier);
+void load(FILE *fichier, Grid *grille, char prenom1[100], char prenom2[100], int *player, int *retrait, int *nb_jetons, int *size) {
+    int l, h;
+    char ligne[100]={0};
+     //Ouverture du fichier
+    fgets(prenom1, 100, fichier);
+    fgets(prenom2, 100, fichier);
+    fgets(ligne, 100, fichier);
+    *player =atoi(ligne);
+    fgets(ligne, 100, fichier);
+    *retrait =atoi(ligne);
+    fgets(ligne, 100, fichier);
+    *nb_jetons =atoi(ligne);
+    fgets(ligne, 100, fichier);
+    *size = atoi(ligne);
+
+    init_grid(grille, *size);
+    for (l = 0; l < grille->largeur; l++) {
+        for (h = 0; h < grille->hauteur; h++) {
+            fgets(&grille->tableau[l][h], 10, fichier);
         }
     }
+
 }
+
 
 void save (Grid grille, int nb_jetons, char prenom1[], char prenom2[],int player, int retrait){
     int l = 0 , h = 0;
